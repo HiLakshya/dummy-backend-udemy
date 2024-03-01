a375adbc11badf679c111d2c674dabad327819fa
HiLakshya <droplakshya@gmail.com>
2024-03-02 00:22:17 +0530

    Adding port from env

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use("/admin", adminRouter)
 app.use("/user", userRouter)
 
-const PORT = 3000;
+const PORT = 3000 || process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
